ba28bd4365848c177314c61a51b2c43f84e1a813
Mateus Galvao <mateustgalvao@outlook.com>
2025-04-07 18:10:21 -0300

    style: use bulma to create form style

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ const registerBtn = document.getElementById("register-btn");
 
 const emailInput = document.getElementById("email-input");
 const passwordInput = document.getElementById("password-input");
+const messageBox = document.getElementById("message-box");
 
-const login = () => {
+const login = (user) => {
     console.log('login');
+    window.location.href = "home.html";
+};
+
+const failedLogin = () => {
+    const failedLoggin = document.createElement("p");
+    failedLoggin.classList.add("has-text-danger");
+    failedLoggin.innerText = "Invalid email or password";
+    messageBox.appendChild(failedLoggin);
 };
 
 const tryLogin = () => {
     const users = JSON.parse(localStorage.getItem("users"));
     const user = users.find(user => user.email === emailInput.value);
     if(!user) {
-        console.log('failed login');
+        failedLogin();
     } else if(user.password === passwordInput.value) {
-        login();
+        login(user);
     } else {
-        console.log('failed login');
+        failedLogin();
     }
 };
 
@@ -27,13 +36,23 @@ const validateEmail = () => {
     return regex.test(email);
 };
 
+const resetFields = () => {
+    emailInput.value = "";
+    passwordInput.value = "";
+    emailInput.placeholder = "user@email.com";
+};
+
 loginBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
     const isValidEmail = validateEmail();
     if(!isValidEmail) {
+        resetFields();
+        emailInput.classList.add("is-danger");
+        emailInput.placeholder = "Invalid email format";
         console.log("invalid email");
     } else {
+        emailInput.classList.remove("is-danger");
         tryLogin();
     }
 });
@@ -41,12 +60,14 @@ loginBtn.addEventListener('click', (event) => {
 registerBtn.addEventListener('click', (event) => {
     event.preventDefault();
     window.location.href = "registration.html";
-    // should redirect to registration page
 });
 
 window.onload = () => {
     localStorage.setItem("users", JSON.stringify([{
         email: "admin@admin.com",
         password: "admin",
+        name: "Admin",
+        surname: "Admin",
+        username: "admin",
     }]));
 };
